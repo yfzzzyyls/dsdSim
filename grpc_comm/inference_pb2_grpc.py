6311b2dc52968e4ede38b2793bb34d62957ef556
Fengze Yu <fy2243@nyu.edu>
2025-04-22 15:22:00 +0000

    still the lsat commit - forget to fix the import issue. my bad
    Signed-off-by: Fengze Yu <fy2243@nyu.edu>

--- grpc_comm/inference_pb2_grpc.py
+++ grpc_comm/inference_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import inference_pb2 as inference__pb2
+from . import inference_pb2 as inference__pb2
 
 GRPC_GENERATED_VERSION = '1.66.2'
 GRPC_VERSION = grpc.__version__
